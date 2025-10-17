cabfecd7d299d9863880b0cbf986c539d852333b
Lana Hendrickson <lhendrickson11@ivytech.edu>
2025-10-17 13:17:26 -0400

    Add files via upload
    temp testing

---
+++ customers_class.py
@@ -0,0 +1,13 @@
+#This is the customers class
+
+class Customers():
+    
+    def __init__(self):
+        self.id_Num = id_Num
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.address = address
+        self.credit_card = credit_card
+        
+        
