227d69311dba7655554fe4bd187fe5a2aa3ba230
LynnO-png <lynno6986@gmail.com>
2025-10-19 20:41:35 -0400

    Update main.py
    Select items using a ComboBox and close a window using the self.master.destroy command

--- main.py
+++ main.py
@@ -10,14 +10,11 @@ from menu_class import Menu
 from customers_class import Customers
 
 #varables
-# firstName = ""
-# lastName = ""
-# email = ""
-# address = ""
-# credit = ""
-#itemName = []
-current_customer = None
-order = None
+firstName = ""
+lastName = ""
+email = ""
+address = ""
+credit = ""
 
 #class
 class windowLogin(EasyFrame):
@@ -50,37 +47,38 @@ class windowLogin(EasyFrame):
     #submit the login infromation to the classes and unlocks the order button
     def submitLoginBtn(self):
         self.menuBtn["state"] = "normal"
-        #global firstName
-        #global lastName
-        #global email
-        #global address
-        #global credit
-        global current_customer
+        global firstName
+        global lastName
+        global email
+        global address
+        global credit
         #add link to customers class
         firstName = self.textFirstName.getText()
         lastName = self.textLastName.getText()
         email = self.textEmail.getText()
         address = self.textAddress.getText()
         credit = self.textCredit.getText()
-        current_customer = Customers(firstName, lastName, email, address, credit)
 
     #opens the order window up
     def menuBtn(self):
         self.destroy()
         windowMenu()
         
-    #opens the order window up
+    #Completely closes the window
     def quitBtn(self):
-        self.destroy()
-        windowQuit()
+        self.master.destroy()
 
-#The window to add items to the order   
+#The window to add items to the order
 class windowMenu(EasyFrame):
-    
+
+    goods = {
+        "Apple Pie" : {"Price" : 10, "Stock" : 60, "Desc" : "Freshly baked apple pie from fresh apples from the local farm."},
+        "White Cake" : {"Price" : 15, "Stock" : 50, "Desc" : "White cake made from a generation-defining recipe."},
+        "Chocolate Cookies" : {"Price" : 5, "Stock" : 35, "Desc" : "Chocolate cookies made with chocolate by our local chocolatier Alex."}
+    }
+    combobox_goods = goods.keys()
+
     def __init__(self):
-        global current_customer
-        global order
-        # get menu items
         with open("menu_items.txt", "rt") as menu_items:
             for item in menu_items:
                 data = item.split(";")
@@ -89,58 +87,50 @@ class windowMenu(EasyFrame):
                 description = data[2]
                 prep_time = int(data[3])
                 Menu(name, price, description, prep_time)
-        
-        order = Order(current_customer)
+            
         EasyFrame.__init__(self, title = "Sweets Cake Bakery")
+        #list of all baked goods
+        goods = {
+        "Apple Pie" : {"Price" : 10, "Stock" : 60, "Desc" : "Freshly baked apple pie from fresh apples from the local farm."},
+        "White Cake" : {"Price" : 15, "Stock" : 50, "Desc" : "White cake made from a generation-defining recipe."},
+        "Chocolate Cookies" : {"Price" : 5, "Stock" : 35, "Desc" : "Chocolate cookies made with chocolate by our local chocolatier Alex."}
+        }
+        combobox_goods = list(goods)
         #first title
         self.addLabel(text = "Order Menu", row = 0, column = 1)
         self.addLabel(text = "These are the available options:", row = 1, column = 0)
         #need to add abilty to print
         self.addLabel(text = Menu.display_item_names(), row = 1, column = 1)
         self.addLabel(text = "Please input item to add:", row = 2, column = 0)
-        self.textMenuAdd = self.addTextField(text="", row = 2, column = 1, width=20)
+        self.comboBoxAdd = self.addCombobox(text = "Placeholder", values = combobox_goods, row = 2, column = 1)
         self.includeBtn = self.addButton(text = "Add Item", row = 3, column = 1, command = self.includeBtn)
         self.removeBtn = self.addButton(text = "Remove Item", row = 4, column = 1, command = self.removeBtn)
         self.orderBtn = self.addButton(text = "Proceed to Checkout", row = 5, column = 1, command = self.orderBtn, state = "disable")
         self.quitBtn = self.addButton(text = "Quit", row = 6, column = 1, command = self.quitBtn)
         
-    #add the item
+    #Combobox selection
     def includeBtn(self):
-        #global itemName
-        global order
+        self.comboBoxAdd["state"] = "normal"
         self.orderBtn["state"] = "normal"
-        #needs to add the abilty to add items to the order.
-        request = self.textMenuAdd.getText()
-        if request in Menu.items:
-            item = Menu.items.get(request)
-            order.add_item(item)
+        #Selecting the item, press the add button to confirm this
+        request = self.comboBoxAdd.get()
+        if request in self.goods:
+            item = Menu.items.update()
             self.messageBox(title = "Add", message = "Your item " + request + " was added")
         else:
-            self.messageBox(title = "Error", message = "Your item " + request + " is not on the menu")
+            self.messageBox(title = "Error", message = "Your item " + request + " was not vaild")
     
     def removeBtn(self):
-        #global itemName
-        global order
         self.orderBtn["state"] = "normal"
-        #needs to add the abilty to add items to the order.
-        request = self.textMenuAdd.getText()
-        if request in Menu.items:
-            item = Menu.items.get(request)
-            if item in order.selected_items:
-                order.remove_item(item)
-                self.messageBox(title = "Removed", message = "Your item " + request + " was removed")
-            else:
-                self.messageBox(title = "Error", message = "Your item " + request + " is not in the order")
-        else:
-            self.messageBox(title = "Error", message = "Your item " + request + " is not on the menu")
+        #need to add the abilty to remove items
 
     def orderBtn(self):
         self.destroy()
         windowOrder()
 
+    #Completely closes the window
     def quitBtn(self):
-        self.destroy()
-        windowQuit()
+        self.master.destroy()
 
 #The window that prints out orders
 class windowOrder(EasyFrame):
@@ -150,32 +140,24 @@ class windowOrder(EasyFrame):
         #first title
         self.addLabel(text = "Your Order", row = 0, column = 1)
         self.addLabel(text = "Your total come out to: ", row = 1, column = 0)
-        self.addLabel(text = order.calculate_price(), row = 1, column = 1)
         #list the total
         self.addLabel(text = "", row = 1, column = 1)
         self.addLabel(text = "It will take about: ", row = 2, column = 0)
         #list the time it will take
-        self.addLabel(text = order.calculate_time(), row = 2, column = 1)
+        self.addLabel(text = "", row = 2, column = 1)
         self.addLabel(text = "This is the customer infromation on the order: ", row = 3, column = 0)
         #List the customer infromation
-        self.addLabel(text = current_customer, row = 3, column = 1)
+        self.addLabel(text = "", row = 3, column = 1)
         self.quitBtn = self.addButton(text = "Submit and Quit", row = 4, column = 1, command = self.quitBtn)
         self.menuBtn = self.addButton(text = "No, Go Back To Menu", row = 6, column = 1, command = self.menuBtn)
 
+    #Completely closes the window
     def quitBtn(self):
-        self.destroy()
-        windowQuit()
+        self.master.destroy()
 
     def menuBtn(self):
         self.destroy()
         windowMenu()
-
-#Respone to quit
-class windowQuit(EasyFrame):
-    
-    def __init__(self):
-        EasyFrame.__init__(self, title = "Sweets Cake Bakery")
-        self.addLabel(text = "Press the 'x' in the top corner to quit", row = 0, column = 1)
   
 #runs the home window
 def main():
@@ -184,4 +166,3 @@ def main():
 #makes main work  
 if __name__ == "__main__":
     main()
-
